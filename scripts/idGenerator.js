f5e59ece0016dbdfa0821b5ae876ef97f92fe3b8
Lissa Goforth <lissagoforth@gmail.com>
2017-11-09 14:23:34 -0600

    created empty nutshellDB, created userfactory, created idGenerator

--- scripts/idGenerator.js
+++ scripts/idGenerator.js
@@ -1,1 +1,11 @@
-//Author:             Purpose: This module will be used by multiple modules to create a unique ID
+//Author: Kevin          Purpose: This module will be used by multiple modules to create a unique ID
+const IdGenerator = function* () {
+    let uniqueId = 1
+
+    while (true) {
+        yield uniqueId
+        uniqueId += 1
+    }
+}
+
+module.exports = IdGenerator
