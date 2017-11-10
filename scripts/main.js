13d52cfbf56f3291ec4da4d2645354bf06d9586f
eagobert <eagobert@hotmail.com>
2017-11-10 14:23:37 -0600

    Create function to save users to local storage

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
 const setVisibility = require("./setVisibility")
 const addeventlisteners = require("./addeventlisteners")
+const localInterface = require("./loadDatabase")
+const nutShellDB = require("./nutShellDB")
+
 setVisibility(document.querySelector(".login"), "show")
+const loadNutshell = localInterface.load("nutShellDB")
