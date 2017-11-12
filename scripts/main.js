3ada6cfbe227f710b65dc8b94ae8246aa30b35fe
eagobert <eagobert@hotmail.com>
2017-11-12 13:12:06 -0600

    commit changes to main.js from last meeting

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,6 @@
 const setVisibility = require("./setVisibility")
 const addeventlisteners = require("./addeventlisteners")
 const localInterface = require("./loadDatabase")
-const nutShellDB = require("./nutShellDB")
 
 setVisibility(document.querySelector(".login"), "show")
 const loadNutshell = localInterface.load("nutShellDB")
