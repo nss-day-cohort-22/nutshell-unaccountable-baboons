2f40461df01dbd53efaed8c490e286552c265698
eagobert <eagobert@hotmail.com>
2017-11-13 20:59:46 -0600

    commit updates to display task module

--- scripts/tasks/markTaskComplete.js
+++ scripts/tasks/markTaskComplete.js
@@ -1,5 +1,13 @@
-//create a checkbox
+//Author:  Erin
+
+//Purpose:  Create an event handler that marks a task complete and then removes the task from the display
+
 
 /*create an event handler that...
     1. marks checkbox complete on click
-    2. changes object status of current task to "complete" */
+    2. changes object property of "complete" to boolean "true"
+    3. removes task from display
+*/
+
+//Create 
+let taskList = document.getElementById()
