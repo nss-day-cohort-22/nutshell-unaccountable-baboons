eb250fe7a139679f5471bbdab3891f1a0e98dfc8
eagobert <eagobert@hotmail.com>
2017-11-14 10:36:17 -0600

    commit changes to displayTask.js

--- scripts/tasks/displayTasks.js
+++ scripts/tasks/displayTasks.js
@@ -3,28 +3,35 @@
 //Purpose:  Module to display task list
 
 /* Required Files*/
-const database = require("../nutShellDB")
+const db = require("../nutShellDB")
 
-//Create a const named "taskListEl" to start the task display
+//Create a const named "taskListEl" to store the task display
 const taskListEl = document.getElementById("taskListElId");
 
-//Create function to show task
-const displayTask = function (){
+//Create function to show task, requiring input of the database
+const displayTasks = function (db){
+
+    //Create const to get the input values of the task name and completion date... (Need class names and add class to input html)
+    const tasks = db.tasks;
+    const taskName = Task_Name.value;
+    const completionDate = Completion_Date.value;
+    const taskId = Task_Id.value;
+
+    //Create const for length of tasks
+    const length = db.tasks[0].length;
 
     //Create a loop to cycle thru each task saved in database OR have each task post at addEvent??
 
-    //Create const to get the input values of the task name and completion date... (Need class names and add class to input html)
-    const taskNameEl = document.getElementsByClassName("taskNameInputClass").value;
-    const completionDateEl = document.getElementsByClassName("completionDateInputClass").value;
+    for (i=0; i < length; i++){
 
-    //and post a check box, task name, and completion date
-    taskListEl.innerHTML = `<ul>
-    <li> ${taskNameEl} + ${completionDateEl} </li>
-    </ul>`;
-
+        //and post a check box, task name, and completion date
+        taskListEl.innerHTML += `<ul>
+            <li> <input type="checkbox" name="complete" value="${tasks.taskId}"> ${tasks.taskName} + ${tasks.completionDate} </li>
+            </ul>`;
+    }
 }
 
 
 
 //Export module for display
-module.exports = displayTask
+module.exports = displayTasks
