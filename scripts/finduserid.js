4e3a7b660518365bc193e2ac1b5e803281522ad2
Kevin Haggerty <kghaggerty@gmail.com>
2017-11-13 18:42:58 -0600

    added chatuserId

--- scripts/finduserid.js
+++
@@ -1,9 +0,0 @@
-const nutshell = require("../nutShellDB")
-
-let nutshellUserArray = nutshell().users[0].userId
-console.log(nutshellUserArray)
-
-    for (let index = 0; index < nutshellUserArray.length; index++) {
-        const element = nutshellUserArray[index];
-        console.log(element, "nutshellarray");
-    }
