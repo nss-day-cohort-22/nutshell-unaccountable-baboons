0c8a427115c30c736261f5d4815c26b718448384
Kevin Haggerty <kghaggerty@gmail.com>
2017-11-15 08:53:29 -0600

    fixed taskId generator

--- scripts/tasks/createNewTask.js
+++ scripts/tasks/createNewTask.js
@@ -3,19 +3,18 @@
  //Purpose: Create new task object to save info passed from addEventHandlerTasks.js
  const nutshell = require("../nutShellDB")
  const idGenerator = require("../idGenerator")
- let lastId = 0
+ let lastid = 0
 
  // Create a new task object with taskName and completion date
  const taskFactory = function (taskName, completionDate) {
-     let db = nutshell();
-     let idMaker = idGenerator(lastId)
-    //
+    let db = nutshell();
+    let idMaker = idGenerator(lastid)
     let tasks = db.tasks;
     let nextId;
     //sorting the users.  If there is no user in local storage, start at one.  Otherwise, look at the last user's userID in local storage and add one to create the new user ID.
     if (tasks.length !== 0) {
-      let sortedtasks = db.tasks.sort((p,n) => n.taskId - p.taskId)
-    nextId = idGenerator(sortedtasks[0].Task_ID)
+      let sortedtasks = db.tasks.sort((p,n) => n.Task_ID - p.Task_ID)
+      nextId = idGenerator(sortedtasks[0].Task_ID).next().value;
     } else if (tasks.length === 0) {
       nextId = idGenerator(0).next().value;
     }
