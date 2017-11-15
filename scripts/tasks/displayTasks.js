cf21f84c15ec8fa660391a2eb4084cf6e0f3ebe3
eagobert <eagobert@hotmail.com>
2017-11-14 18:08:41 -0600

    Commit new changes to displayTasks.js

--- scripts/tasks/displayTasks.js
+++ scripts/tasks/displayTasks.js
@@ -14,32 +14,33 @@ const database =loadDatabase();
 //Create function to show task, requiring input of the database
 const displayTasks = function (database){
 
-    //conditional statement to target/retrieve the currentuser
+        //conditional statement to target/retrieve the currentuser
 
-    //Create const to get the input values of the task name and completion date... (Need class names and add class to input html)
-    let tasks = database.tasks;
-    console.log(tasks[0].Task_Name);
-
-    //Create a loop to cycle thru each task saved in database
-    tasks.forEach(function(task){
-
-        //Testing the loop...
-        console.log("Yaay! The task loop is working");
-        //Create a const named "taskListEl" to store the task display
+        //Create const to get the input values of the task name and completion date... (Need class names and add class to input html)
+        let tasks = database.tasks
         const taskListEl = document.getElementsByClassName("display_tasks")[0];
-        const length = tasks.length;
-        console.log(length);
+        //const length = tasks.length
+        if (tasks > 0) {
+        //Create a loop to cycle thru each task saved in database
+        tasks.forEach(function(task){
 
-        //and post a check box, task name, and completion date
-        taskListEl.innerHTML += `
-            <li> <input type="checkbox" name="complete" value="${tasks[0].Task_ID}"> 
-            ${tasks[0].Task_Name} ${tasks[0].Completion_Date} </li>
-            `;
-    });
+            //Testing the loop...
+            console.log("Yaay! The task loop is working");
+            //Create a const named "taskListEl" to store the task display
+            //console.log(length);
 
-}
+            //and post a check box, task name, and completion date
+            taskListEl.innerHTML += `
+                <li> <input type="checkbox" name="complete" value="${tasks[0].Task_ID}"> 
+                ${tasks[0].Task_Name} ${tasks[0].Completion_Date} </li>
+                `;
+        });
+        } else {
+        console.log("you have no tasks");
+            }
+    }
 
-displayTasks(database);
+    displayTasks(database);
 
-//Export module for display
-module.exports = displayTasks
+    //Export module for display
+    module.exports = displayTasks
